91dcac18933d83b5641ff4807762938a8f879008
laienguang <laienguang@gmail.com>
2026-07-29 15:21:08 +0800

    Initial Commit

---
+++ filego/filegoApp.swift
@@ -0,0 +1,17 @@
+//
+//  filegoApp.swift
+//  filego
+//
+//  Created by 赖恩光 on 2026/7/29.
+//
+
+import SwiftUI
+
+@main
+struct filegoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
